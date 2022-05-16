67b897fcb97ca7c6769c880a51ac7b6ceef42304
dungnv9 <nguyendungbk92@gmail.com>
2022-05-16 15:13:29 +0700

    init

---
+++ src/main/java/com/dung/demoopenshift/DemoOpenShiftApplication.java
@@ -0,0 +1,13 @@
+package com.dung.demoopenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOpenShiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOpenShiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dung/demoopenshift/DemoOpenShiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dung.demoopenshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOpenShiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
